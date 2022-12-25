663b220ca7dd6426aa8bfa2d0c311d7f45d894f2
qhuy22601 <qhuy22601@gmail.com>
2022-12-25 10:40:33 +0700

    final

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
     </React.StrictMode>
   </Provider>,
 );
-registerServiceWorker();
+// registerServiceWorker();
 
 
 // If you want to start measuring performance in your app, pass a function
